b2571f2cc0c1848c90a6267d6cf3757978348e5a
Unknown <umbaga@gmail.com>
2017-10-10 19:05:44 -0400

    Changed DndButton

--- src/components/common/DndButton.js
+++ src/components/common/DndButton.js
@@ -2,101 +2,113 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
-const DndButton = ({onClick, buttonType, isBadge, additionalButtonStyles}) => {
-    let bootstrapStyle = '';
-    let fontawesomeStyle = 'fa fa-';
-    let extraText = '';
-    let extraClasses = '';
-    if (isBadge) {
-        extraClasses += ' badge ';
+class DndButton extends React.Component {
+    constructor(props, context) {
+        super(props, context);
     }
-    extraClasses += ' ' + additionalButtonStyles;
-    switch (buttonType.toLowerCase()) {
-        case 'additem':
-            fontawesomeStyle += 'plus-circle';
-            bootstrapStyle += 'default';
-            break;
-        case 'back':
-            fontawesomeStyle += 'angle-left';
-            bootstrapStyle += 'default';
-            break;
-        case 'cancel':
-            fontawesomeStyle += 'ban';
-            bootstrapStyle += 'primary';
-            break;
-        case 'collapse':
-            fontawesomeStyle += 'caret-up';
-            bootstrapStyle += 'default';
-            break;
-        case 'create':
-            fontawesomeStyle += 'file-text-o';
-            bootstrapStyle += 'primary';
-            break;
-        case 'delete':
-            fontawesomeStyle += 'trash-o';
-            bootstrapStyle += 'primary';
-            break;
-        case 'edit':
-            fontawesomeStyle += 'edit';
-            bootstrapStyle += 'primary';
-            break;
-        case 'expand':
-            fontawesomeStyle += 'caret-down';
-            bootstrapStyle += 'default';
-            break;
-        case 'hamburger':
-            fontawesomeStyle += 'bars';
-            bootstrapStyle += 'primary';
-            break;
-        case 'movedown':
-            fontawesomeStyle += 'caret-down';
-            bootstrapStyle += 'default';
-            break;
-        case 'moveup':
-            fontawesomeStyle += 'caret-up';
-            bootstrapStyle += 'default';
-            break;
-        case 'removeitem':
-            fontawesomeStyle += 'minus-circle';
-            bootstrapStyle += 'default';
-            break;
-        case 'reset':
-            fontawesomeStyle += 'undo';
-            bootstrapStyle += 'primary';
-            break;
-        case 'save':
-            fontawesomeStyle += 'floppy-o';
-            bootstrapStyle += 'primary';
-            break;
-        case 'savenew':
-            fontawesomeStyle += 'floppy-o';
-            bootstrapStyle += 'primary';
-            extraText += '+';
-            break;
-        case 'search':
-            fontawesomeStyle += 'search';
-            bootstrapStyle += 'default';
-            break;
-        case 'view':
-            fontawesomeStyle += 'eye';
-            bootstrapStyle += 'primary';
-            break;
-        default:
-            fontawesomeStyle += 'cog';
-            bootstrapStyle += 'primary';
+
+    render() {
+        let bootstrapStyle = '';
+        let fontawesomeStyle = 'fa fa-';
+        let extraText = '';
+        let extraClasses = '';
+        if (this.props.isBadge) {
+            extraClasses += ' badge ';
+        }
+        extraClasses += ' ' + this.props.additionalButtonStyles;
+        switch (this.props.buttonType.toLowerCase()) {
+            case 'additem':
+                fontawesomeStyle += 'plus-circle';
+                bootstrapStyle += 'default';
+                break;
+            case 'back':
+                fontawesomeStyle += 'angle-left';
+                bootstrapStyle += 'default';
+                break;
+            case 'cancel':
+                fontawesomeStyle += 'ban';
+                bootstrapStyle += 'primary';
+                break;
+            case 'collapse':
+                fontawesomeStyle += 'caret-up';
+                bootstrapStyle += 'default';
+                break;
+            case 'create':
+                fontawesomeStyle += 'file-text-o';
+                bootstrapStyle += 'primary';
+                break;
+            case 'delete':
+                fontawesomeStyle += 'trash-o';
+                bootstrapStyle += 'primary';
+                break;
+            case 'edit':
+                fontawesomeStyle += 'edit';
+                bootstrapStyle += 'primary';
+                break;
+            case 'expand':
+                fontawesomeStyle += 'caret-down';
+                bootstrapStyle += 'default';
+                break;
+            case 'hamburger':
+                fontawesomeStyle += 'bars';
+                bootstrapStyle += 'primary';
+                break;
+            case 'movedown':
+                fontawesomeStyle += 'caret-down';
+                bootstrapStyle += 'default';
+                break;
+            case 'moveup':
+                fontawesomeStyle += 'caret-up';
+                bootstrapStyle += 'default';
+                break;
+            case 'removeitem':
+                fontawesomeStyle += 'minus-circle';
+                bootstrapStyle += 'default';
+                break;
+            case 'reset':
+                fontawesomeStyle += 'undo';
+                bootstrapStyle += 'primary';
+                break;
+            case 'save':
+                fontawesomeStyle += 'floppy-o';
+                bootstrapStyle += 'primary';
+                break;
+            case 'savenew':
+                fontawesomeStyle += 'floppy-o';
+                bootstrapStyle += 'primary';
+                extraText += '+';
+                break;
+            case 'search':
+                fontawesomeStyle += 'search';
+                bootstrapStyle += 'default';
+                break;
+            case 'view':
+                fontawesomeStyle += 'eye';
+                bootstrapStyle += 'primary';
+                break;
+            default:
+                fontawesomeStyle += 'cog';
+                bootstrapStyle += 'primary';
+        }
+        const renderedLabel = (this.props.label && this.props.label.length != 0 && this.props.buttonType.toLowerCase() == 'label') ? (
+            <div>{this.props.label}</div>
+        ) : (
+            <div><i className={fontawesomeStyle + extraClasses}></i>{extraText}</div>
+        );
+        return (
+            <Button bsStyle={bootstrapStyle} onClick={this.props.onClick}>
+                {renderedLabel}
+            </Button>
+        );
     }
-    return (
-        <Button bsStyle={bootstrapStyle} onClick={onClick}>
-            <i className={fontawesomeStyle + extraClasses}></i>{extraText}
-        </Button>
-    );
-};
+}
 
 DndButton.propTypes = {
     buttonType: PropTypes.string.isRequired,
     onClick: PropTypes.func,
     isBadge: PropTypes.bool,
-    additionalButtonStyles: PropTypes.string
+    additionalButtonStyles: PropTypes.string,
+    label: PropTypes.string
 };
 
 export default DndButton;
